87639b5d345648464f31c89c46fa17d1a7019a2d
De - Nice Onu Peter <85217880+deniceonupeter@users.noreply.github.com>
2021-07-21 16:05:47 +0200

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+let myName = 'De - Nice';
+document.write(myName);
+myName = 'Onu Peter';
+document.write(myName);
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
